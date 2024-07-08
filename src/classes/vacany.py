e80fc830dc6e581bacf482c16e520aeaa0cae740
Елена Прилуцкая <a.priluckay87@gmail.com>
2024-07-08 20:08:00 +0300

    add method from_hh_dict to class Vacancy

--- src/classes/vacany.py
+++ src/classes/vacany.py
@@ -9,3 +9,23 @@ class Vacancy:
         self.salary_to = salary_to
         self.salary_currency = salary_currency
         self.link = link
+
+    @classmethod
+    def from_hh_dict(cls, data):
+        """
+        метод создает экземпляры вакансий с заданными атрибутами
+        :param data: json файл
+        :return: cls
+        """
+        name = data.get("name")
+        description = data.get("snippet").get("requirement")
+        salary = data.get("salary")
+        salary_from = 0
+        salary_to = 0
+        salary_currency = None
+        if salary:
+            salary_from = salary.get("from", 0) if salary.get("from") is not None else 0
+            salary_to = salary.get("to", 0) if salary.get("to") is not None else 0
+            salary_currency = salary.get("currency")
+        link = data.get("alternate_url")
+        return cls(name, description, salary_from, salary_to, salary_currency, link)
