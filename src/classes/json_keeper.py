b65cf98b095b78c28c9d3e09b9c992ebd10c6529
Елена Прилуцкая <a.priluckay87@gmail.com>
2024-07-08 20:14:52 +0300

    class JsonKeeper

---
+++ src/classes/json_keeper.py
@@ -0,0 +1,19 @@
+from src.classes.data_keeper import DataKeeper
+import json
+
+
+class JSONKeeper(DataKeeper):
+    """
+    Клаасс для сохранения информации о вакансиях в JSON-файл
+    """
+    def __init__(self, file_name):
+        super().__init__(file_name)
+
+    def load_data(self, vacancies):
+        """
+        Метод для записи информации в JSON-файл
+        :param vacancies: экземпляры класса
+        :return: JSON-файл
+        """
+        with open(self.file_name, "w", encoding='utf-8') as file:
+            file.write(json.dumps(vacancies, indent=2, ensure_ascii=False))
